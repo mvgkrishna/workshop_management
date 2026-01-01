f9d418ab497a8910e6c06cc2ee27fc6709b177b5
Krishna <gkcomputers21@gmail.com>
2026-01-01 20:58:34 +0530

    working on users grid

--- src/pages/admin/usersManagement/UsersManagement.jsx
+++ src/pages/admin/usersManagement/UsersManagement.jsx
@@ -1,13 +1,60 @@
 import CustomDatagrid from "../../../customComponents/datagrid/CustomDatagrid";
+import CustomPaper from "../../../customComponents/paper/CustomPaper";
+import CustomTypography from "../../../customComponents/typography/CustomTypography";
 import SideNav from "../../../layout/sidenav/SideNav";
+import { rows, columns } from "../../../mockData/usersGridData";
+import CustomTextField from "../../../customComponents/textfield/CustomTextfield";
+import CustomButton from "../../../customComponents/button/CustomButton";
+import SearchIcon from "@mui/icons-material/Search";
+import BorderColorRoundedIcon from "@mui/icons-material/BorderColorRounded";
+import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
 
 import "./UserManagement.css";
+import { InputAdornment } from "@mui/material";
 
 const UserManagement = () => {
   return (
     <div className="UserManagement_container">
       <SideNav />
-      <CustomDatagrid />
+      <CustomPaper>
+        <div className="header_options">
+          <CustomTypography
+            calssName="heading"
+            sx={{
+              fontFamily: "Poppins, sans-serif",
+              fontSize: "24px",
+              fontWeight: 800,
+              color: "#1f2937",
+              padding: "17px",
+            }}
+          >
+            User Management
+          </CustomTypography>
+          <div className="usermanagement_searchOptions">
+            <CustomTextField
+              placeholder="search..."
+              slotProps={{
+                input: {
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                },
+              }}
+            ></CustomTextField>
+            <CustomButton variant="contained">Add user</CustomButton>
+          </div>
+        </div>
+        <CustomDatagrid
+          rows={rows}
+          columns={columns}
+          pageSize={10}
+          pageSizeOptions={[5]}
+          checkboxSelection={false}
+          disableRowSelectionOnClick
+        />
+      </CustomPaper>
     </div>
   );
 };
